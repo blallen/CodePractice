b4fc7e6e2929c7f75232ad0cd7294012addba098
Brandon Allen <ballen@mit.edu>
2021-03-25 12:23:05 -0400

    use pandas and make plots for iris test

--- python/iris_test.py
+++ python/iris_test.py
@@ -2,47 +2,65 @@ import numpy
 import pandas
 import sklearn
 import matplotlib.pyplot as plot
-import matplotlib.patches as mpatches
 
-from sklearn import impute
-from sklearn import preprocessing
-from sklearn import pipeline
-from sklearn import compose
-from sklearn import model_selection
-from sklearn import metrics
-from sklearn import ensemble
 from sklearn import datasets
+from sklearn import model_selection
+from sklearn import pipeline
+from sklearn import ensemble
+from sklearn import metrics
 
+
+# load iris into data frame
 iris = datasets.load_iris()
 
-print(type(iris)) # can convert to data frame
+df = pandas.DataFrame(iris.data, columns = iris.feature_names)
+df['target'] = pandas.Series(iris.target)
+df['species'] = pandas.Categorical.from_codes(iris.target, iris.target_names)
 
-X = iris['data']
-y = iris['target']
+# show class distribution
+print("Number of each target class is ")
+print(df.groupby('target').size())
 
-print(X.shape)
-print(y.shape)
+# output features
+print(df.head())
+
+# plot distributions
+# plot X[i] vs y
+# plot X[i] vs X[j]
+
+def plot_scatter(x, y, df):
+    fig = plot.figure()
+
+    plot.scatter(x, y, data = df, c = 'target')
+
+    plot.xlabel(x)
+    plot.ylabel(y)
+
+    fig.savefig('plots/' + x + '_vs_' + y + '.pdf',  bbox_inches='tight')
+
+for i in range(0, len(iris.feature_names)):
+    x = iris.feature_names[i]
+    # plot_scatter(x, 'target')
+
+    for j in range(i+1, len(iris.feature_names)):
+        y = iris.feature_names[j]
+        plot_scatter(x, y, df)
+    # end
+# end
+
+# create feature and target dataframes
+features = df.drop(['target', 'species'], axis = 1)
+target = df['target']
 
 # for regression 
 # do pipeline with PCA
 # and be able to show it satisfies the four requirement
 
-# show class distribution
-# output features
-
-# plot X[i] vs y
-# plot X[i] vs X[j]
-
-# df = pandas.DataFrame(data = X, columns = ['one', 'two', 'three', 'four'])
-# df['target'] = y
-
-# print(df.head())
-
 # talk about missing values
 # normalization
 # onehot encoding
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, random_state = 0)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(features, target, random_state = 0)
 
 rfc = ensemble.RandomForestClassifier(
     n_estimators = 100,
@@ -51,11 +69,9 @@ rfc = ensemble.RandomForestClassifier(
 )
 
 # mention pipeline
-
-rfc.fit(X_train, y_train)
-
 # do cross validation
 
+rfc.fit(X_train, y_train)
 y_pred = rfc.predict(X_test)
 
 report = metrics.classification_report(y_test, y_pred)
